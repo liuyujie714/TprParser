e070d342b29ce593ae6640064579dd47126a775c
liuyujie714 <1051690424@qq.com>
2024-02-28 11:09:19 +0800

    add test

--- TprParser/main.cpp
+++ TprParser/main.cpp
@@ -4,14 +4,13 @@ int main(int argc, char *argv[])
 {
 	try
 	{
-		//TprReader reader("inter-md.tpr");
-		//TprReader reader(argv[1]);
-		TprReader reader("md2024.tpr");
-		//TprReader reader("2023demo.tpr");
-		//TprReader reader("2022.tpr");
-		//TprReader reader("2lyz_gmx_2021.tpr");
-		//TprReader reader("2020.4_gra.tpr");
-		//TprReader reader2("2020.4_gra.tpr");
+		//TprReader reader("test/inter-md.tpr");
+		TprReader reader1("test/md2024.tpr");
+		//TprReader reader2("test/2023demo.tpr");
+		//TprReader reader3("test/2022.tpr");
+		//TprReader reader4("test/2lyz_gmx_2021.tpr");
+		//TprReader reader5("test/2020.4_gra.tpr");
+		//TprReader reader6("test/2020.4_gra.tpr");
 	}
 	catch (const std::exception&e)
 	{
