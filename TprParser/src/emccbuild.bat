4af323a6ef8843f89f115c990896fc74df0372de
liuyujie714 <1051690424@qq.com>
2025-02-14 01:04:56 +0800

    add js wrapper from emcc

---
+++ TprParser/src/emccbuild.bat
@@ -0,0 +1,7 @@
+@echo off
+call "C:\Users\liuyujie714\Desktop\emsdk\emsdk_env.bat"
+
+rem emcc main.cpp Reader.cpp Utils.cpp -o test.html -sNO_DISABLE_EXCEPTION_CATCHING --preload-file .\test\benchBFC_FEP.tpr && emrun test.html
+
+emcc -lembind -O2 Reader.cpp Utils.cpp emcc.cpp -o libgmxtpr.js -sNO_DISABLE_EXCEPTION_CATCHING -sFORCE_FILESYSTEM -sEXPORTED_RUNTIME_METHODS=['FS'] -lworkerfs.js -sWASM=0
+
